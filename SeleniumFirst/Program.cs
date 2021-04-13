77300250579bf06b59324e1f1a25350cdfbf2fb3
Rafael Garzon <Rafael.Garzon@endava.com>
2021-04-13 10:40:29 -0500

    Agregar archivos de proyecto.

---
+++ SeleniumFirst/Program.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace SeleniumFirst
+{
+    
+    class Program
+    {
+        //Create the reference for our browser
+        readonly IWebDriver driver = new ChromeDriver(@"C:\Users\rgarzon\source\repos\SeleniumFirst\SeleniumFirst\drivers\");
+
+        //static void Main(string[] args)
+        //{   
+        //}
+
+        [SetUp]
+        public void Inicialize()
+        {
+            //Navigate to Google page
+            driver.Navigate().GoToUrl("http://executeautomation.com/demosite/index.html?UserName=&Password=Login=Login");
+            Console.WriteLine("Opened URL");
+        }
+
+        [Test]
+        public void ExecuteTest()
+        {
+            //Find The Element
+            IWebElement element = driver.FindElement(By.Name("q"));
+
+            //Perform Ops
+            element.SendKeys("executeautomation");
+
+            Console.WriteLine("Executed Test");
+            
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
+            Console.WriteLine("Closed the driver");
+        }
+    }
+}
---
+++ SeleniumFirst/SeleniumSetMethods.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumFirst
+{
+    class SeleniumSetMethods
+    {
+        //Enter Text
+        public static void EnterText(IWebDriver driver, String element, string value, string elementType)
+        {
+            if(elementType == "Id")
+                driver.FindElement(By.Id(element)).SendKeys(value);
+            if (elementType == "Name")
+                driver.FindElement(By.Name(element)).SendKeys(value);
+        }
+    }
+}
